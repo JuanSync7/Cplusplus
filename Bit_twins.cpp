616063dd9a3cc6996fcba3edf2d9958d09097866
JuanSync7 <118364808+JuanSync7@users.noreply.github.com>
2023-07-03 13:49:22 +0100

    bit twins

---
+++ Bit_twins.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+#include <bitset>
+#include <cmath>
+
+using namespace std;
+
+int string_to_int(const string& decimal_num){
+    int len = decimal_num.length();
+    int integer = 0;
+    float temp;
+    for (int i = 0; i < len; i++) {
+        temp = (int(decimal_num[i]) - '0')*pow(10,i);
+        integer += temp;      
+    }
+    
+    cout << "int:" << integer << endl;
+    return integer;
+}
+
+string decimal_to_binary(const int& integer, const int& bit_size){
+    char binary[bit_size];
+    int temp = integer;
+    cout << "temp:" << temp << endl;
+    for(int i = bit_size-1; i >= 0; i--){
+        binary[i] = char(temp%(2) + '0');
+        temp = temp/2;
+        cout << "temp:"<< temp << endl;
+    }
+    
+    cout << "binary:" << binary << endl;
+    return binary;
+}
+
+int countBitTwins(const string& binaryNumber) {
+    int count = 0;
+    int index = 0;
+    int len = binaryNumber.length();
+    while (index < len - 3) {
+        if (binaryNumber.substr(index, 4) == "0110") {
+            count++;
+        }
+        index++;
+    }
+    return count;
+}
+
+int main() {
+    string decimalNumber;
+    while (getline(cin, decimalNumber)) {
+        // Convert decimal number to 32-bit binary representation
+        //string binaryNumber = bitset<32>(stoi(decimalNumber)).to_string();
+
+        string binaryNumber = decimal_to_binary(string_to_int(decimalNumber),32);
+        // Count bit-twins in the binary representation
+        int twinsCount = countBitTwins(binaryNumber);
+
+        // Print the result
+        cout << "Number of bit-twins: " << twinsCount << endl;
+        return 0;
+    }
+
+   
+}
